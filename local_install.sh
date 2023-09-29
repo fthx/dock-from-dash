3c5bf1c9ba2d7290abcb69807f617ca4cce377b5
fthx <23138504+fthx@users.noreply.github.com>
2023-09-29 21:52:44 +0200

    Delete local_install.sh

--- local_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf ~/.local/share/gnome-shell/extensions/dock-from-dash@fthx/*
-rm -rf .build
-mkdir .build
-meson --prefix=$HOME/.local/ --localedir=share/gnome-shell/extensions/dock-from-dash@fthx/locale .build
-ninja -C .build install
-rm -rf .build
